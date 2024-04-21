bffdb632c03a36cf96fd09b8ca0aafcf1ed4bc78
Carlos Victor Galdino Oliveira <71177748+Carlos-TM@users.noreply.github.com>
2024-04-21 18:25:12 +0000

    adicionado script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function Home() {
-    return <><h1>Carlos Oliveir DEV</h1><h2>Teste de texto</h2></>
+  return (
+    <>
+      <h1>Carlos Oliveir DEV</h1>
+      <h2>Teste de texto</h2>
+    </>
+  );
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
 export default Home;
